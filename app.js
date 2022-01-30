7a886b52dc36a42603dce2bf5913b16aaef8568e
ravindu <rwofficial98@gmail.com>
2022-01-30 23:29:40 +0530

    Level 4 - Salting and Hashing passwords with bcrypt

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 //const encrypt = require('mongoose-encryption');
-const md5 = require("md5");
+//const md5 = require("md5");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
+
 
 const app = express();
 
@@ -23,9 +27,15 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
+//Level 2 --> encryption
 //use the secret for encrypt our database
 //get the secret from .env file
 //userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
+//Level 3 --> hashing
+//console.log("weak password hash: " + md5("123456"));
+//console.log("strong password hash: " + md5("asdrdsffsgs678DFGSTGF343434@__gdgsh"));
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,9 +53,11 @@ app.get("/login", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
     const newUser = new User({
       email: req.body.username,
-      password: md5(req.body.password)
+      password: hash
     });
 
     newUser.save(function(err) {
@@ -57,9 +69,12 @@ app.post("/register", function(req, res) {
     });
   });
 
+
+  });
+
   app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}, function(err, foundUser) {
       if(err) {
@@ -67,9 +82,19 @@ app.post("/register", function(req, res) {
       } else {
         //initial password that user registered must to be equal to the login password that user type
         //compare the hash that is inside our database with the hashed version of their password
-        if(foundUser.password === password) {
-          res.render("secrets");
-        }
+        // if(foundUser.password === password) {
+        //   res.render("secrets");
+        // }
+
+        //check our login password is correct
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          //check password after hashing with the salt is equal to the hash that we have got stored in our database
+          if(result === true) {
+            res.render("secrets");
+          } else {
+            res.redirect("/login");
+          }
+        });
       }
     });
   });
